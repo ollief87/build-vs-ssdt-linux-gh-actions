6dc1181154dca187ecaebb7aec4c5188fb4fd901
Ollie Flavel <ollie.flavel@insight.com>
2024-03-03 14:26:10 +1100

    Initial SDK project

---
+++ src/EmployeeDbSdk/Employee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Employee]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [FirstName] NVARCHAR(200) NOT NULL,
+    [LastName] NVARCHAR(200) NOT NULL,
+    [Email] NVARCHAR(200) NOT NULL
+);
